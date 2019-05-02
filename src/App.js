3fc7f121631a5304c9a620aae355a99adfbb0eab
Paul Breslin <paul.breslin@outlook.com>
2019-05-02 09:16:12 +0100

    Adds app styles

--- src/App.js
+++ src/App.js
@@ -4,25 +4,32 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import ExchangeRates from './pages/exchange-rates';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/about">
-          <li>About</li>
-        </Link>
-        <Link to="/exchange-rates">
-          <li>Exchange rates</li>
-        </Link>
-      </nav>
-      <Route path="/" exact component={Home} />
-      <Route path="/about" exact component={About} />
-      <Route path="/exchange-rates" exact component={ExchangeRates} />
+      <div className={styles.app}>
+        <main className={styles.content}>
+          <h1>Exchange Rates</h1>
+          <nav className={styles.nav}>
+            <ul>
+              <Link to="/">
+                <li>Home</li>
+              </Link>
+              <Link to="/about">
+                <li>About</li>
+              </Link>
+              <Link to="/exchange-rates">
+                <li>Exchange rates</li>
+              </Link>
+            </ul>
+          </nav>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/exchange-rates" exact component={ExchangeRates} />
+        </main>
+      </div>
     </Router>
   );
 }
